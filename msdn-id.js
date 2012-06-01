97cd8af4360e71c8d7d15eea6e7e2cbfb86dcaf0
Alasdair North <alnorth@gmail.com>
2012-06-01 08:45:33 +0100

    Store the value in the database.

--- msdn-id.js
+++ msdn-id.js
@@ -5,30 +5,42 @@ var http = require("http"),
 
 function getIDFromMSDN(shortId, callback) {
     http.get({"host": "msdn.microsoft.com", port: 80, path: "/en-us/library/" + shortId}, function(res) {
-        var body = "";
+        if(res.statusCode == 200) {
+            var body = "";
 
-        res.on('data', function (chunk) {
-            body += chunk;
-        });
-        res.on('end', function (chunk) {
-            matches = body.match(/<link rel="canonical" href="http:\/\/msdn\.microsoft\.com\/en-us\/library\/([a-zA-Z0-9.]+)\.aspx" \/>/);
-            try {
-                callback(matches[1]);
-            } catch (err) {
-                console.log(err);
-                callback(null);
-            }
-        });
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
+            res.on('end', function (chunk) {
+                matches = body.match(/<link rel="canonical" href="http:\/\/msdn\.microsoft\.com\/en-us\/library\/([a-zA-Z0-9.]+)\.aspx" \/>/);
+                try {
+                    callback(matches[1]);
+                } catch (err) {
+                    console.log(err);
+                    callback(null);
+                }
+            });
+        } else {
+            callback(null);
+        }
     }).on('error', function(e) {
         console.log("Got error: " + e.message);
         callback(null);
     });
 }
 
-function getIDWithDB(shortId, db, callback) {
-
-    getIDFromMSDN(shortId, function(canonical) {
-        callback(canonical);
+function getIDWithDB(shortId, ids, callback) {
+    ids.findOne({"short_id": shortId}, function(err, item) {
+        if(item) {
+            callback(item.canonical);
+        } else {
+            getIDFromMSDN(shortId, function(canonical) {
+                if(canonical) {
+                    ids.insert({"short_id": shortId, "canonical": canonical});
+                }
+                callback(canonical);
+            });
+        }
     });
 }
 
@@ -38,11 +50,16 @@ var db = new Db("msdn-ids", server);
 db.open(function(err, db) {
     if(!err) {
 
-        getIDWithDB("ms149618", db, function(canonical) {
-            console.log(canonical);
+        db.collection("ids", function(err, ids) {
+            if(!err) {
+                getIDWithDB("8hftfeyw", ids, function(canonical) {
+                    console.log(canonical);
+                    db.close();
+                });
+            } else {
+                console.log("MongoDB error", err);
+            }
         });
-
-        db.close();
 
     } else {
         console.log("MongoDB error", err);
